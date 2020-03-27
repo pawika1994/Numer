41d41788acfb8cbdc9cffb08783693dcec14c502
pawika1994 <62654515+pawika1994@users.noreply.github.com>
2020-03-27 19:34:14 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,34 +9,12 @@ import Falseposition from './components/root/Falseposition';
 import Newton from './components/root/Newton';
 import Onepoint from './components/root/Onepoint';
 import Secant from './components/root/Secant';
-import Eulermethod from './components/OrdinaryDiff/Eulermethod';
-import Heunmethid from './components/OrdinaryDiff/Heunmethod';
-import ModifiedEuler from './components/OrdinaryDiff/ModifiedEuler';
 import FirstBackward from './components/NumericalDiff/FirstBackward';
 import FirstCentral from './components/NumericalDiff/FirstCentral';
 import FirstForward from './components/NumericalDiff/FirstForward';
 import SecondBackward from './components/NumericalDiff/SecondBackward';
 import SecondCentral from './components/NumericalDiff/SecondCentral';
 import SecondForward from './components/NumericalDiff/SecondForward';
-import LinearRegression from './components/LSR/LinearRegression';
-import MultipleRegression from './components/LSR/MultipleRegression';
-import PolynormialRegression from './components/LSR/PolynormialRegression';
-import Cholesky from './components/LinearAlgebraic/Cholesky';
-import ConjugateGradient from './components/LinearAlgebraic/ConjugateGradient';
-import CramerRule from './components/LinearAlgebraic/CramerRule';
-import GaussianElimination from './components/LinearAlgebraic/GaussianElimination';
-import GaussJordanMethod from './components/LinearAlgebraic/GaussJordanMethod';
-import GaussSeidel from './components/LinearAlgebraic/GaussSeidel';
-import Jacobi from './components/LinearAlgebraic/Jacobi';
-import LU from './components/LinearAlgebraic/LU';
-import MatrixInversionMethod from './components/LinearAlgebraic/MatrixInversionMethod';
-import LagrangeLinear from './components/interpolation/LagrangeLinear';
-import LagrangeQuadratic from './components/interpolation/LagrangeQuadratic';
-import LinearSpline from './components/interpolation/LinearSpline';
-import NewtonsLinear from './components/interpolation/NewtonsLinear';
-import NewtonsPolynomial from './components/interpolation/NewtonsPolynomial';
-import NewtonsQuadratic from './components/interpolation/NewtonsQuadratic';
-import QuadraticSpline from './components/interpolation/QuadraticSpline';
 import CompositeSimpson from './components/IntegrationandDiff/CompositeSimpson';
 import CompositeTrapezoidal from './components/IntegrationandDiff/CompositeTrapezoidal';
 import SimpsonRule from './components/IntegrationandDiff/SimpsonRule';
@@ -51,9 +29,6 @@ ReactDOM.render(
         <Route path="/Newton" component={Newton} />
         <Route path="/Onepoint" component={Onepoint} />
         <Route path="/Secant" component={Secant} />
-        <Route path="/Eulermethod" component={Eulermethod}/>
-        <Route path="/Heunmethid" component={Heunmethid} />
-        <Route path="/ModifiedEuler" component={ModifiedEuler} />
         <Route path="/FirstBackward" component={FirstBackward} />
         <Route path="/FirstCentral" component={FirstCentral} />
         <Route path="/FirstForward" component={FirstForward} />
@@ -61,25 +36,7 @@ ReactDOM.render(
         <Route path="/SecondBackward" component={SecondBackward} />
         <Route path="/SecondCentral" component={SecondCentral} />
         <Route path="/SecondForward" component={SecondForward} />
-        <Route path="/LinearRegression" component={LinearRegression} />
-        <Route path="/MultipleRegression" component={MultipleRegression} />
-        <Route path="/PolynormialRegression" component={PolynormialRegression} />
-        <Route path="/Cholesky" component={Cholesky} />
-        <Route path="/ConjugateGradient" component={ConjugateGradient} />
-        <Route path="/CramerRule" component={CramerRule} />
-        <Route path="/GaussianElimination" component={GaussianElimination} />
-        <Route path="/GaussJordanMethod" component={GaussJordanMethod} />
-        <Route path="/GaussSeidel" component={GaussSeidel} />
-        <Route path="/Jacobi" component={Jacobi} />
-        <Route path="/LU" component={LU} />
-        <Route path="/MatrixInversionMethod" component={MatrixInversionMethod} />
-        <Route path="/LagrangeLinear" component={LagrangeLinear} />
-        <Route path="/LagrangeQuadratic" component={LagrangeQuadratic} />
-        <Route path="/LinearSpline" component={LinearSpline} />
-        <Route path="/NewtonsLinear" component={NewtonsLinear} />
-        <Route path="/NewtonsPolynomial" component={NewtonsPolynomial} />
-        <Route path="/NewtonsQuadratic" component={NewtonsQuadratic} />
-        <Route path="/QuadraticSpline" component={QuadraticSpline} />
+        
         <Route path="/CompositeSimpson" component={CompositeSimpson} />
         <Route path="/CompositeTrapezoidal" component={CompositeTrapezoidal} />
         <Route path="/SimpsonRule" component={SimpsonRule} />
