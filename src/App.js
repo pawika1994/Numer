c9837e0c8b1336f13378921b15cedaa13621868c
pawika1994 <62654515+pawika1994@users.noreply.github.com>
2020-03-27 19:32:52 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,116 +50,6 @@ class App extends React.Component {
 
                         <SubMenu title={
                             <span>
-                                <span>Linear Algebraic Equations</span>
-                            </span>
-                        }
-                        >
-                                <Menu.Item key='CramerRule'>
-                                <a href="/CramerRule">Cramer’s Rule</a>
-                                 </Menu.Item>
-                                <Menu.Item key='GaussianElimination'>
-                                <a href="/GaussianElimination">Gaussian Elimination</a>    
-                                </Menu.Item>
-                                <Menu.Item key='GaussJordanMethod'>
-                                <a href="/GaussJordanMethod">Gauss Jordan Method</a> 
-                                 </Menu.Item>
-                                <Menu.Item key='MatrixInversionMethod'>
-                                <a href="/MatrixInversionMethod">Matrix Inversion Method</a>    
-                                </Menu.Item>
-                                <Menu.Item key='LU'>
-                                <a href="/LU">LU Decomposition Method</a> 
-                                </Menu.Item>
-                                <Menu.Item key='Cholesky'>
-                                <a href="/Cholesky">Cholesky Method</a>
-                                </Menu.Item>
-                                <Menu.Item key='Jacobi'>
-                                <a href="/Jacobi">Jacobi Iteration Method</a>
-                                </Menu.Item>
-                                <Menu.Item key='GaussSeidel'>
-                                <a href="/GaussSeidel">Gauss-Seidel Iteration Method</a>
-                                </Menu.Item>
-                                <Menu.Item key='ConjugateGradient'>
-                                <a href="/ConjugateGradient">Conjugate Gradient Method</a>
-                                </Menu.Item>
-                        </SubMenu>
-
-                        <SubMenu
-                            title={
-                                <span>
-                                    <span>Interpolation and Extrapolation</span>
-                                </span>
-                            }
-                        >
-                            <Menu.ItemGroup key='Newtons divided-differences' title='Newtons divided-differences'>
-                                <Menu.Item key='NewtonsLinear'>
-                                <a href="/NewtonsLinear">Linear Interpolation</a>
-                                </Menu.Item>
-                                <Menu.Item key='NewtonsQuadratic'>
-                                <a href="/NewtonsQuadratic">Quadratic Newton</a>
-                                </Menu.Item>
-                                <Menu.Item key='NewtonsPolynomial'>
-                                <a href="/NewtonsPolynomial">Polynomial Newton</a>
-                                </Menu.Item>
-                            </Menu.ItemGroup>
-
-                            <Menu.ItemGroup key='Lagrange polynomials' title='Lagrange polynomials'>
-                                <Menu.Item key='LagrangeLinear'>
-                                <a href="/LagrangeLinear">Linear Interpolation</a>
-                                </Menu.Item>
-                                <Menu.Item key='LagrangeQuadratic'>
-                                <a href="/LagrangeQuadratic">Quadratic Newton</a>
-                                </Menu.Item>
-                            </Menu.ItemGroup>
-
-                            <Menu.ItemGroup key='Spline interpolation' title='Spline interpolation'>
-                                <Menu.Item key='LinearSpline'>
-                                <a href="/LinearSpline">Linear Spline</a>
-                                </Menu.Item>
-                                <Menu.Item key='QuadraticSpline'>
-                                <a href="/QuadraticSpline">Quadratic Spline</a>
-                                </Menu.Item>
-                            </Menu.ItemGroup>
-                        </SubMenu>
-
-                        <SubMenu title={
-                            <span>
-                                <span>Least Squares Regression</span>
-                            </span>
-                        }>
-                            <Menu.Item key='LinearRegression'>
-                            <a href="/LinearRegression">Linear Regression</a>
-                            </Menu.Item>
-                            <Menu.Item key='PolynormialRegression'>
-                            <a href="/PolynormialRegression">Polynormial Regression</a>
-                            </Menu.Item>
-                            <Menu.Item key='MultipleRegression'>
-                            <a href="/MultipleRegression">Multiple Regression</a>
-                            </Menu.Item>
-                        </SubMenu>
-
-                        <SubMenu title={
-                            <span>
-                                <span>Integration and Differentiation</span>
-                            </span>
-                        }>
-                            <Menu.ItemGroup>
-                                <Menu.Item key='TrapezoidalRule'>
-                                <a href="/TrapezoidalRule">Trapezoidal Rule</a>
-                                </Menu.Item>
-                                <Menu.Item key='CompositeTrapezoidal'>
-                                <a href="/CompositeTrapezoidal">Composite Trapezoidal</a>
-                                </Menu.Item>
-                                <Menu.Item key='SimpsonRule'>
-                                <a href="/SimpsonRule">Simpson Rule</a>
-                                </Menu.Item>
-                                <Menu.Item key='CompositeSimpson'>
-                                <a href="/CompositeSimpson">Composite Simpson</a>
-                                </Menu.Item>
-                            </Menu.ItemGroup>
-                        </SubMenu>
-
-                        <SubMenu title={
-                            <span>
                                 <span>Numerical Differentiation</span>
                             </span>
                         }>
@@ -187,21 +77,6 @@ class App extends React.Component {
                             </Menu.ItemGroup>
                         </SubMenu>
 
-                        <SubMenu title={
-                            <span>
-                                <span>Ordinary Differential Equation</span>
-                            </span>
-                        }>
-                            <Menu.Item key='Eulermethod'>
-                            <a href="/Eulermethod">Euler's method</a>
-                            </Menu.Item>
-                            <Menu.Item key='Heunmethod'>
-                            <a href="/Heunmethod">Heun's method</a>
-                            </Menu.Item>
-                            <Menu.Item key='ModifiedEuler'>
-                            <a href="/ModifiedEuler">Modified Euler's method</a>
-                            </Menu.Item>
-                        </SubMenu>
                     </Menu>
                 </Sider>
                 <Layout>
